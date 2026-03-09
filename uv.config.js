188ea613d5f793e89229e5aeedd7a76adf987715
Glaeesas <Sparefornow2026@outlook.com>
2026-03-09 17:37:57 -0400

    Update uv.config.js

--- uv.config.js
+++ uv.config.js
@@ -1,1 +1,10 @@
-
+self.__uv$config = {
+  prefix: '/uv/service/',
+  bare: 'https://uv.radon.games/bare/',
+  encodeUrl: Ultraviolet.codec.xor,
+  decodeUrl: Ultraviolet.codec.xor,
+  handler: '/uv/uv.handler.js',
+  bundle: '/uv/uv.bundle.js',
+  config: '/uv/uv.config.js',
+  sw: '/uv.sw.js',
+};
